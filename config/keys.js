c45ab344486a0ca3d4c49f8a32e125c7477e1c33
prudvi-m <69066646+prudvi-m@users.noreply.github.com>
2021-05-24 22:51:04 +0530

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // mongoURI: 'mongodb://brad:brad@ds231725.mlab.com:31725/devconnector',
+  mongoURI:'mongodb+srv://Prudvi:comeonatngu@databank.nl1vw.mongodb.net/devconnector',
+  secretOrKey: 'secret'
+};
