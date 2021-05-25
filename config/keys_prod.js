60026160ff1e4aba2ebd4eafc02feea6b665c1c6
prudvi-m <69066646+prudvi-m@users.noreply.github.com>
2021-05-25 21:50:47 +0530

    connection string

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,7 +1,7 @@
 module.exports = {
-  // mongoURI: process.env.MONGO_URI,
-  // secretOrKey: process.env.SECRET_OR_KEY
-  mongoURI:'mongodb+srv://Prudvi:comeonatngu@databank.nl1vw.mongodb.net/devconnector',
+  //mongoURI: 'mongodb://brad:brad@ds231725.mlab.com:31725/devconnector',
+  //mongoURI:'mongodb+srv://Prudvi:comeonatngu@databank.nl1vw.mongodb.net/devconnector',
+  mongoURI:'mongodb+srv://Abhinav:Abhipa1@test.mprxk.mongodb.net/Logistics',
   //mongoURI:'mongodb://127.0.0.1:27017/devconnector',
   secretOrKey: 'secret'
 };
