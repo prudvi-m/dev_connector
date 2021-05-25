5b89f04d3c7d2ecb4d61073fd42efbbd1d2b1bc7
prudvi-m <69066646+prudvi-m@users.noreply.github.com>
2021-05-25 09:19:40 +0530

    deprecated, connection, & cors

--- server.js
+++ server.js
@@ -5,22 +5,25 @@ const passport = require('passport');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+const cors = require('cors');
 
 const app = express();
 
 // DB Config
 const db = require('./config/keys').mongoURI;
 
+
+app.use(cors());
+
 // Connect to MongoDB
 mongoose
-  .connect(db)
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+  .catch(err => console.log(' internal mongoose error', err));
 
 // Body parser middleware
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(bodyParser.json());
-
 
 // Passport middleware
 app.use(passport.initialize());
@@ -39,6 +42,17 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
