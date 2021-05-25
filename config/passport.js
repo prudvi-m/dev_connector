307c106ff493a9ecb2498c9dc613f1ca641a6405
prudvi-m <69066646+prudvi-m@users.noreply.github.com>
2021-05-25 20:52:29 +0530

    compared bugs for deployment

--- config/passport.js
+++ config/passport.js
@@ -12,7 +12,12 @@ module.exports = passport => {
   passport.use(
     new JwtStrategy(opts, (jwt_payload, done) => {
       User.findById(jwt_payload.id)
-        .then(user =>  done(null, user ?? false))
+        .then(user => {
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
+        })
         .catch(err => console.log(err));
     })
   );
