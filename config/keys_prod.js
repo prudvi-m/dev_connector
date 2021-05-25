9e630a65747e8e4edb91ebcb417e56c15adcc2dc
prudvi-m <69066646+prudvi-m@users.noreply.github.com>
2021-05-25 20:59:15 +0530

    temperory env values

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,7 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY
+  // mongoURI: process.env.MONGO_URI,
+  // secretOrKey: process.env.SECRET_OR_KEY
+  mongoURI:'mongodb+srv://Prudvi:comeonatngu@databank.nl1vw.mongodb.net/devconnector',
+  //mongoURI:'mongodb://127.0.0.1:27017/devconnector',
+  secretOrKey: 'secret'
 };
